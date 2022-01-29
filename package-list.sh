2844b776bef5b2fbd91584d7c3792066550383f7
Netfloex <38650595+Netfloex@users.noreply.github.com>
2022-01-29 02:38:02 +0100

    Updates for gnome, Splitted root,Packages install

---
+++ package-list.sh
@@ -0,0 +1,54 @@
+## Applications
+
+ferdi-bin
+discord
+visual-studio-code-bin
+# bitwarden
+teamviewer
+libreoffice-fresh
+ark unrar p7zip
+vlc
+
+## Utils
+
+# Cli
+yarn npm
+micro bat exa
+github-cli
+headsetcontrol
+bottom htop ctop nvtop
+docker-compose docker
+
+gnome-keyring
+sshfs
+xbindkeys xdotool
+flameshot
+net-tools inetutils
+
+kdeconnect
+
+## Flutter
+
+# android-studio
+# flutter
+
+## Games
+multimc-bin
+
+# wine
+# winetricks
+# legendary
+
+## Drivers
+# nvidia-dkms-performance
+
+## Fonts
+ttf-fira-code
+ttf-borg-sans-mono
+ttf-font-awesome
+
+cronie
+
+# Gnome
+gnome-shell
+equilux-theme
