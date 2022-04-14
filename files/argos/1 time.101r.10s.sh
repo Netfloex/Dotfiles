d87eea95d7b64c954b3a704a4231a9b8cd590418
Netfloex <38650595+Netfloex@users.noreply.github.com>
2022-04-14 15:09:47 +0200

    Use gsconnect for battery information

--- files/argos/1 time.101r.10s.sh
+++ files/argos/1 time.101r.10s.sh
@@ -3,6 +3,6 @@
 
 TIME=$(date +"%R")
 
-echo $TIME
+echo "$TIME | size=20"
 echo "---"
 echo "Shutdown | bash='shutdown 0' terminal=false iconName=system-shutdown-symbolic"
--- files/argos/3 phone.82r.10s.sh
+++ files/argos/3 phone.82r.10s.sh
@@ -1,55 +1,22 @@
 
-DEVICES=$(
-    gdbus call -e \
-    -d org.kde.kdeconnect \
-    -o /modules/kdeconnect \
-    -m org.kde.kdeconnect.daemon.devices
-)
-# 				Remove these characters  ][)(',
-DEVICE_ID=$(echo "$DEVICES" | sed -r "s|[][)(',]||g")
-
+DEVICE_ID=$(~/.local/share/gnome-shell/extensions/gsconnect@andyholmes.github.io/service/daemon.js -l)
 
 BATTERY=$(
     gdbus call -e \
-    -d org.kde.kdeconnect \
-    -o /modules/kdeconnect/devices/$DEVICE_ID/battery \
-    -m org.freedesktop.DBus.Properties.GetAll \
-    org.kde.kdeconnect.device.battery \
+    -d org.gnome.Shell.Extensions.GSConnect \
+    -o /org/gnome/Shell/Extensions/GSConnect/Device/$DEVICE_ID \
+    -m org.gtk.Actions.Describe battery \
     2>&1
 )
-
 STATUS=$?
-PERCENTAGE=$(echo "$BATTERY" | sed "s|[^0-9]*||g")
+PERCENTAGE=$(echo "$BATTERY" | cut -d"," -f5)
+ICON=$(echo "$BATTERY" | cut -d"'" -f4)
 
-
-if [ $STATUS -eq 1 ] || [ $PERCENTAGE -eq 1 ]
+if [ $STATUS -eq 1 ]
 then
-    echo " | iconName=battery-missing-symbolic"
+    echo "| iconName=battery-missing-symbolic"
     exit
 fi
 
-[[ "$BATTERY" == *"false"* ]]
-CHARGING=$?
 
-CHARGING_STRING="-"
-if [ $CHARGING -eq 1 ]
-then
-    CHARGING_STRING="-charging-"
-fi
-
-ICON_STRING="full"
-
-if (($PERCENTAGE < 15))
-then
-    ICON_STRING="caution"
-elif (($PERCENTAGE < 25))
-then
-    ICON_STRING="low"
-elif (($PERCENTAGE < 60))
-then
-    ICON_STRING="good"
-fi
-
-
-
-echo "$PERCENTAGE% | iconName=battery-${ICON_STRING}${CHARGING_STRING}symbolic"
+echo "$PERCENTAGE% | iconName=${ICON}"
